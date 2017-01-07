7c4df197465076191715d7174185741b9b42068b
Palash Patidar <palashpatidar08@gmail.com>
2017-01-07 12:09:08 +0530

    First commit

---
+++ locbot/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.conf.urls import include
+from locbot import views
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+
+urlpatterns = [
+	
+    url(r'^find/$',views.use_api, name="search"),
+    url(r'^$', views.home, name = "homepage"),
+]
+
---
+++ locbot/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse
+import requests
+import json
+#credentials
+app_id = 'd91b9bb0'
+app_key = '3de79c03abb64b024def260427502bcd'
+
+language = 'en'
+#word_id = 'Ace'
+# Create your views here.
+def home(request):
+	context = {'latest_question_list': "latest_question_list"}
+	return render(request, 'index.html', context)
+
+def use_api(request):
+	response = request.GET.get('key')
+	url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + response.lower()
+
+	r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+
+	print("code {}\n".format(r.status_code))
+	print r.content
+	#print("json \n" + json.dumps(r.json()))
+	return HttpResponse(r.content)
